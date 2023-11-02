3e236c1674c839c7b7fde7a4dfce781433a500f3
Akhan <93190613+Dulatbay@users.noreply.github.com>
2023-11-03 02:52:15 +0600

    configure and create keycloak

--- app/user-ms/src/main/java/com/example/userms/config/SecurityConfig.java
+++ app/user-ms/src/main/java/com/example/userms/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,9 +13,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/main/*", "/home").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/main/*", "/home")
+                        .permitAll()
+                        .anyRequest()
+                        .permitAll()
                 );
+
 
         return http.build();
     }
--- app/user-ms/src/main/java/com/example/userms/controller/TestController.java
+++ app/user-ms/src/main/java/com/example/userms/controller/TestController.java
@@ -1,14 +1,20 @@
 package com.example.userms.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("main")
+@RequestMapping("/main")
 public class TestController {
-    @GetMapping("test")
+
+
+    @Value("${eureka.instance.instance-id}")
+    private String port;
+
+    @GetMapping("/test")
     public String getTest(){
-        return "test";
+        return port;
     }
 }
